94d4ed0ac729dbab2546f2677d25418ac0501b07
wawzysys <1916040083@qq.com>
2024-01-18 16:00:29 +0800

    测试

--- acw/test.java
+++ acw/test.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-public class test{
-	public static void main(String args[]){
+// import java.util.Scanner;
+
+public class test {
+	public static void main(String args[]) {
 		System.out.println("hell");
 	}
 }
-
-
-Hello, this is a snippet. 
