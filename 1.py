d3b483448be8e2f89fc43d442d0eb0e1fcacd3bc
wawzysys <1916040083@qq.com>
2025-03-16 00:13:24 +0800

    init

---
+++ 1.py
@@ -0,0 +1,60 @@
+# E:\0Code\Algorithm\1.py 2025-03-15 by 777
+import sys
+sys.setrecursionlimit(100000)
+input=lambda:sys.stdin.readline().strip()
+# write=lambda x:sys.stdout.write(str(x)+'\n')
+# from decimal import Decimal
+# from datetime import datetime,timedelta
+# from random import randint
+# from copy import deepcopy
+from collections import *
+# from heapq import heapify,heappush,heappop
+# from bisect import bisect_left,bisect,insort
+# from math import inf,sqrt,gcd,pow,ceil,floor,log,log2,log10,pi,sin,cos,tan,asin,acos,atan
+# from functools import cmp_to_key,reduce
+# from operator import or_,xor,add,mul
+# from itertools import permutations,combinations,accumulate
+sint = lambda: int(input())
+mint = lambda: map(int, input().split())
+lint = lambda: list(map(int, input().split()))
+
+def solve():
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    #t=int(input())
+    #for _ in range(t):
+    #   solve()
+
+    solve()
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.n = n
+        self.setCount = n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x == y:
+            return False
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        self.parent[y] = x
+        self.size[x] += self.size[y]
+        self.setCount -= 1
+        return True
+
+    def connected(self, x, y):
+        x, y = self.find(x), self.find(y)
+        return x == y
